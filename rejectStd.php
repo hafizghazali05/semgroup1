45735682869d9437ce0748f18dfc65e66bf12e90
efatihah <56181461+efatihah@users.noreply.github.com>
2019-11-21 18:16:45 +0800

    Approve hostel application

---
+++ rejectStd.php
@@ -0,0 +1,19 @@
+<html>
+<body>
+<?php 
+
+    include('config.php');
+
+		$id = $_GET['id'];
+				
+		$sql = "UPDATE application SET status='REJECTED' WHERE id='$id'";
+				
+		mysqli_query($con, $sql); 
+
+		echo '<script type="text/javascript">
+		alert("Successfully Update!");
+		window.location = "approveStd.php";
+		</script>';
+?>
+</body>
+</html>
