87ac85c57cccbfe535dd584c2355c37f417658a8
MUHAMMAD AZIZI FAHMI BIN ABD RANI <49760891+AziziFahmiAr@users.noreply.github.com>
2019-12-11 01:20:57 +0800

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php 
 error_reporting(1);
-$con = mysqli_connect("localhost","root","","hostel");
-
+$con=mysqli_connect("localhost","azizifah_admin","P@ssw0rd");
+mysqli_select_db($conn,"azizifah_hostel");
 ?>
